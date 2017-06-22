d3a3ae0a6f96b7970970faccd5b0fb6e38b2a2d1
Joseph Ladino <drtnt25@gmail.com>
2017-06-22 14:14:12 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,6 +5,7 @@
   $dbusername = $dbparts['user'];
   $dbpassword = $dbparts['pass'];
   $dbname = ltrim($dbparts['path'],'/');
+  echo $dbhostname.' '.$dbhostname.' '.$dbpassword.' '.$name
   //make connection to mysql
   $dbc = mysqli_connect($dbhostname, $dbusername, $dbpassword, $dbname) or die('could not connect to database');
   //set encoding
