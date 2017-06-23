b0c52da945278853e238afb70877af0974720414
Joseph Ladino <drtnt25@gmail.com>
2017-06-22 20:23:27 -0400

    Updated connection for two databases
    ....

--- connect.php
+++ connect.php
@@ -1,10 +1,16 @@
 <?php
-  $url = getenv('CLEARDB_DATABASE_URL');
-  $dbparts = parse_url($url);
+  echo $_SERVER['REQUEST_URI'];
+  if(strpos($_SERVER['REQUEST_URI'], 'my_projects')) {
+    $dbparts = array('host'=>'localhost', 'user'=>'root', 'pass'=>'', 'path'=>'practice');
+    $dbname = $dbparts['path'];
+  } else {
+    $url = getenv('CLEARDB_DATABASE_URL');
+    $dbparts = parse_url($url);
+    $dbname = ltrim($dbparts['path'],'/');
+  }
   $dbhostname = $dbparts['host'];
   $dbusername = $dbparts['user'];
   $dbpassword = $dbparts['pass'];
-  $dbname = ltrim($dbparts['path'],'/');
   //make connection to mysql
   $dbc = mysqli_connect($dbhostname, $dbusername, $dbpassword, $dbname) or die('could not connect to database');
   //set encoding
