d0673c44c8463a936e61d8d4bcf63edc8c54b190
Joseph-Ladino <drtnt25@gmail.com>
2017-06-24 23:58:24 -0400

    Working on chatroom

--- chatroom1.php
+++ chatroom1.php
@@ -1,3 +1,14 @@
 <?php
+  include('curnt_usr.php');
+?>
 
- ?>
+
+<html>
+  <head>
+    <title>Chatroom</title>
+    <?php
+      $fp = fopen('chatroom1.php', 'a');
+      fwrite($fp, '<h1>Welcome to the chatroom, '.$current_user.'</h1>');
+    ?>
+  </head>
+</html>
